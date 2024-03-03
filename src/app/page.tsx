29f987e4e6f8ab96e552f226db2d0ce4c62c0210
hidetomo-shimaoka <hyde2000s@gmail.com>
2024-03-03 15:06:29 -0700

    update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default async function Home() {
     <div className="p-4">
       <div className="mb-8">
         <img alt="logo" className="w-48" src="/logo.png" />
-        <div>This is PFP Meme Transformer on Farcaster Frame.</div>
       </div>
     </div>
   );
